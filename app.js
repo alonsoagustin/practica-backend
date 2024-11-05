60940d0004722d87cae42c960479e500553a64ad
Agustin Alonso <alonsoiagustin@gmail.com>
2024-11-05 23:22:06 +0100

    feat: :sparkles: Set up userRouter for /users endpoint

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Import the Express library
 const express = require('express');
 
+// Import the user route module
+const userRouter = require('./3_routes/userRoute');
+
 // Create an instance of an Express application
 const app = express();
+
+// Routes
+// Define the routes for user-related operations
+app.use('/users', userRouter);
 
 // Export the Express application for use in other modules
 module.exports = app;
