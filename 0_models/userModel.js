f4dc1b2155889d437e260c4c424efcea49fac5a0
Agustin Alonso <alonsoiagustin@gmail.com>
2024-11-05 23:24:42 +0100

    fix: :bug: Correct property errors in user model

--- 0_models/userModel.js
+++ 0_models/userModel.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose'); // Import Mongoose, which is a library for working with MongoDB
-const validator = require('validator'); // Import the validator library for validating data
+// Import Mongoose, which is a library for working with MongoDB
+const mongoose = require('mongoose');
+
+// Import the validator library for validating data
+const validator = require('validator');
 
 // Define the user schema using Mongoose
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please tell us your name'],
-    maxlength: [5, 'A user name must have less or equal then 5 characters'],
-    minlength: [50, 'A user name must have more or equal then 50 characters'],
+    minlength: [5, 'A user name must have less or equal then 5 characters'],
+    maxlength: [50, 'A user name must have more or equal then 50 characters'],
     lowercase: true,
     trim: true, // Remove whitespace from both ends
     unique: true, // The name value must be unique in the collection
@@ -25,13 +28,10 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please provide your password'],
     minlength: [8, 'A password must have at least 8 characters'],
   },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-  },
 });
 
 // Create a user model based on the defined schema, which allows interaction with the 'users' collection in MongoDB.
 const User = mongoose.model('User', userSchema);
 
-module.exports = User; // Export the model for use in other parts of the application
+// Export the model for use in other parts of the application
+module.exports = User;
