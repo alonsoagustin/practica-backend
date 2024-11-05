2069366f835a946aca7115d816fdb104334e1f31
Agustin Alonso <alonsoiagustin@gmail.com>
2024-11-05 23:18:58 +0100

    feat: :sparkles: Define POST route for user signup

--- 3_routes/userRoute.js
+++ 3_routes/userRoute.js
@@ -7,5 +7,8 @@ const authController = require('./../2_controllers/authController');
 // Create a new router instance from Express
 const router = express.Router();
 
+// Define a POST route for user signup that triggers the signup method in the authController
+router.post('/signup', authController.signup);
+
 // Export the router for use in other modules
 module.exports = router;
